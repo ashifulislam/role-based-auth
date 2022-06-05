e3413fe04af5e45f512a2a6d15b6bd170dbf3c18
princeashiful <princeashifulislam@gmail.com>
2022-06-05 18:50:48 +0600

    role based auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,37 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected o$redirectT = RouteServiceProvider::HOME;
+
+    public function redirectTo()
+    {
+        switch (Auth::user()->role)
+        {
+            case 1:
+                $this->redirectTo = "/admin";
+                return $this->redirectTo;
+                break;
+            
+            case 2:
+                $this->redirectTo = "/manager";
+                return $this->redirectTo;
+                break;
+            
+            case 3:
+                $this->redirectTo = "/user";
+                return $this->redirectTo;
+                break;
+
+                default:
+                $this->redirectTo = "/login";
+                return $this->redirectTo;
+
+                break;
+
+            
+
+        }
+    }
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin','AdminController@index')->name('admin')->middleware('admin');
+Route::get('/manager','ManagerController@index')->name('manager')->middleware('manager');
+Route::get('/user','UserController@index')->name('user')->middleware('user');
